ff4286c61c61ef2053601842e0f1b4687f2977b5
Jhean Santana de Miranda <jheanpequenotalento@gmail.com>
2025-09-22 10:49:35 -0300

    Add files via upload

---
+++ ex3/ex3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero = int.Parse(Console.ReadLine()!);
+
+        if (numero % 2 == 0)
+            Console.WriteLine("PAR");
+        else
+            Console.WriteLine("ÍMPAR");
+    }
+}
---
+++ ex_4/ex_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 10; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
