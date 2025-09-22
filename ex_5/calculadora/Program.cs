8b0050ab3a572d91edb3befa0a2c3bee9b7c4fb0
Jhean Santana de Miranda <jheanpequenotalento@gmail.com>
2025-09-22 10:50:51 -0300

    Add files via upload

---
+++ ex_5/calculadora/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o raio do círculo: ");
+        double raio = double.Parse(Console.ReadLine()!);
+
+        double area = Math.PI * Math.Pow(raio, 2);
+
+        Console.WriteLine("A área do círculo é: " + area);
+    }
+}
