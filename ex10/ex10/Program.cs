ff3274173dae4dd02b2ccfc858d9dda8c90c1d5b
Jhean Santana de Miranda <jheanpequenotalento@gmail.com>
2025-09-22 10:53:39 -0300

    Add files via upload

---
+++ ex10/ex10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma palavra ou frase: ");
+        string texto = Console.ReadLine()!;
+
+        Console.Write("Digite a letra que deseja contar: ");
+        char letra = char.Parse(Console.ReadLine()!);
+
+        int contador = 0;
+
+        for (int i = 0; i < texto.Length; i++)
+        {
+            if (texto[i] == letra)
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine($"A letra '{letra}' aparece {contador} vez(es).");
+    }
+}
---
+++ ex9/ex9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma palavra: ");
+        string texto = Console.ReadLine()!;
+
+        string invertida = "";
+
+        for (int i = texto.Length - 1; i >= 0; i--)
+        {
+            invertida += texto[i];
+        }
+
+        Console.WriteLine("Invertida: " + invertida);
+    }
+}
